8f6e3871214b1643dc4022b2f28611c31da8e756
fuggfuggfugg <pwskhisty1234>
2015-02-01 16:47:46 -0500

    cleanup

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,10 @@
+'use strict';
 // jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
-    if ($(".navbar").offset().top > 250) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    if ($('.navbar').offset().top > 250) {
+        $('.navbar-fixed-top').addClass('top-nav-collapse');
     } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        $('.navbar-fixed-top').removeClass('top-nav-collapse');
     }
 });
 
@@ -11,7 +12,7 @@ $(window).scroll(function() {
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
-        $('html, body').stop().velocity("scroll", { offset:  $($anchor.attr('href')).offset().top, mobileHA: false });
+        $('html, body').stop().velocity('scroll', { offset:  $($anchor.attr('href')).offset().top, mobileHA: false });
         
         event.preventDefault();
     });
